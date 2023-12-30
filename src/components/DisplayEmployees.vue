c6441b16f0437e1773a1d04da65d4ad9ae6c5921
AlliiAbbas <eng.aliiabbas@gmail.com>
2023-12-30 03:55:07 +0200

    Create static page for showing employees data

--- src/components/DisplayEmployees.vue
+++ src/components/DisplayEmployees.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="w-full bg-gray-100">
-    <div>
+  <div class="w-full bg-gray-100 grid grid-cols-12 height">
+    <div class="col-span-12 md:col-span-6 grid align-middle justify-center">
+      <div class="rounded-3xl overflow-hidden m-5">
+        <img src="https://img.freepik.com/free-photo/handsome-businessman-offering-handshake_144627-28925.jpg?w=740&t=st=1703897360~exp=1703897960~hmac=6cb2ec0cc54fd942cb6cf70336517d477a3b4aa30bb0fd3dba0cb8ccfc62bc32" alt="employee-img" width="410"/>
+      </div>
     </div>
-    <div>
-
+    <div class="col-span-12 md:col-span-6">
+      <div class="w-full text-center p-5 mt-5 text-5xl">
+        <h1>Employee Data</h1>
+      </div>
+      <div class="text-3xl">
+        <p class="font-bold my-12">Name: <span class="font-light">ali</span></p>
+        <p class="font-bold my-12">Position: <span class="font-light">ali</span></p>
+        <p class="font-bold my-12">Department: <span class="font-light">ali</span></p>
+        <p class="font-bold my-12">Email: <span class="font-light">ali</span></p>
+      </div>
     </div>
   </div>
 </template>
@@ -11,5 +22,11 @@
 <script>
 export default {
 
-}
+} 
 </script>
+
+<style>
+.height{
+  height: calc(100vh - 70px);
+}
+</style>
